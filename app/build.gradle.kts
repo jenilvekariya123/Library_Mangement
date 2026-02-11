03ae9660069a5e6e484caf31ab8825d7291c6e7a
jenilvekariya12 <johnmakram253@gmail.com>
2026-02-11 22:53:24 +0000

    commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
@@ -33,8 +35,11 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-    kotlinOptions {
-        jvmTarget = "1.8"
+
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_1_8)
+        }
     }
 
     buildFeatures {
@@ -75,10 +80,6 @@ dependencies {
 
     // Glide for image loading
     implementation (libs.glide)
-
-    val lottieVersion = "3.4.0"
-    implementation (libs.lottie)
-
 
 
 }
